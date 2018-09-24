b53fd72673fbbb27a255c56ea23578bceb56ec47
NaokiHori <poker714youtube@yahooo.co.jp>
2018-09-24 22:25:53 +0200

    [mod] install variable

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 USERHOME=/home/naokihori
 INSTALL=/usr/local
+GITVER=2.19.0
+PYTHONVER=3.7.0
 
 # git
 yum remove -y git
@@ -11,9 +13,9 @@ yum install -y autoconf
 yum install -y wget
 yum install -y curl-devel expat-devel gettext-devel  openssl-devel zlib-devel perl-ExtUtils-MakeMaker
 cd $INSTALL/src
-wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.18.0.tar.gz
-tar zxvf git-2.18.0.tar.gz
-cd git-2.18.0
+wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-$GITVER.tar.gz
+tar zxvf git-$GITVER.tar.gz
+cd git-$GITVER
 make configure
 ./configure --prefix=$INSTALL
 make all
@@ -21,7 +23,7 @@ make install
 echo 'export PATH=$PATH:/usr/local/bin' >> $USERHOME/.bash_profile
 source $USERHOME/.bash_profile
 yum remove -y git
-rm -rf $INSTALL/src/git-2.18.0 $INSTALL/src/git-2.18.0.tar.gz
+rm -rf $INSTALL/src/git-$GITVER $INSTALL/src/git-$GITVER.tar.gz
 
 # python3
 git clone https://github.com/pyenv/pyenv.git $USERHOME/.pyenv
@@ -30,8 +32,8 @@ echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> $USERHOME/.bash_profile
 echo 'eval "$(pyenv init -)"' >> $USERHOME/.bash_profile
 source $USERHOME/.bash_profile
 yum install -y bzip2 bzip2-devel readline-devel sqlite sqlite-devel libffi-devel
-pyenv install 3.7.0
-pyenv global 3.7.0
+pyenv install $PYTHONVER
+pyenv global $PYTHONVER
 echo 'export PATH="$PYENV_ROOT/versions/3.7.0:$PATH"' >> $USERHOME/.bash_profile
 source $USERHOME/.bash_profile
 
@@ -41,7 +43,7 @@ yum install -y lua-devel
 cd /usr/local/src
 git clone https://github.com/vim/vim
 cd vim
-./configure  --enable-fail-if-missing  --with-features=huge  --enable-python3interp=dynamic  --with-python3-config-dir=/home/naokihori/.pyenv/versions/3.7.0/lib/python3.7/config-3.7m-x86_64-linux-gnu/  --enable-cscope  --enable-fontset  --enable-multibyte  vi_cv_path_python3=/home/naokihori/.pyenv/versions/3.7.0/bin/python3.7  --enable-luainterp=dynamic  --enable-gpm  --enable-luainterp
+./configure --enable-fail-if-missing --with-features=huge --enable-cscope --enable-fontset  --enable-multibyte --enable-luainterp=dynamic --enable-gpm --enable-luainterp
 make
 make install
 echo 'alias vi="vim"' >> /home/naokihori/.bashrc
